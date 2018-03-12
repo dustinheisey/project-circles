6a6f540606c92d9d915e79131f6c778a83541f79
Dustin Heisey <dustinheisey23@gmail.com>
2018-03-12 14:41:02 -0500

    Added Some Reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,47 @@
 <template>
   <div id="app">
     <h1 class="heading" id="heading">{{heading}}</h1>
-    <div class="item img-2"><img src="./assets/circle.jpg" class="img" id="1" alt="Circle"></div>
-    <div class="item img-1"><img src="./assets/circles.jpg" class="img" id="2" alt="Circles"></div>
-    <div class="item img-9"><img src="./assets/triangle.jpg" class="img" id="3" alt="Triangle"></div>
-    <div class="item img-11"><img src="./assets/triangles.jpg" class="img" id="4" alt="Triangles"></div>
-    <div class="item img-4"><img src="./assets/square.jpg" class="img" id="5" alt="Square"></div>
-    <div class="item img-10"><img src="./assets/squares.jpg" class="img" id="6" alt="Squares"></div>
-    <div class="item img-3"><img src="./assets/pentagon.jpg" class="img" id="7" alt="Pentagon"></div>
-    <div class="item img-8"><img src="./assets/pentagons.jpg" class="img" id="8" alt="Pentagons"></div>
-    <div class="item img-7"><img src="./assets/hexagon.jpg" class="img" id="9" alt="Hexagon"></div>
-    <div class="item img-6"><img src="./assets/hexagons.jpg" class="img" id="10" alt="Hexagons"></div>
-    <div class="item img-5"><img src="./assets/octagon.jpg" class="img" id="11" alt="Octagon"></div>
-    <div class="item img-12"><img src="./assets/octagons.jpg" class="img" id="12" alt="Octagons"></div>
+    <div v-for="(item, index) in art" :key="index" class="item" :class="'img-' + (index + 1)">
+      <a :href="item.src" download>
+        <img :src="item.src" :alt="item.heading">
+      </a>
+    </div>
   </div>
 </template>
 
 <script>
+const path = "../static/assets/";
 export default {
   name: "app",
   data() {
     return {
-      headings: [
-        "Circle",
-        "Circles",
-        "Triangle",
-        "Triangles",
-        "Square",
-        "Squares",
-        "Pentagon",
-        "Pentagons",
-        "Hexagon",
-        "Hexagons",
-        "Octagon",
-        "Octagons"
-      ],
-      heading: "Circles"
+      heading: "Circles",
+      art: [
+        { heading: "circle", src: path + "circle.jpg" },
+        { heading: "circles", src: path + "circles.jpg" },
+        { heading: "triangle", src: path + "triangle.jpg" },
+        { heading: "triangles", src: path + "triangles.jpg" },
+        { heading: "square", src: path + "square.jpg" },
+        { heading: "squares", src: path + "squares.jpg" },
+        { heading: "pentagon", src: path + "pentagon.jpg" },
+        { heading: "pentagons", src: path + "pentagons.jpg" },
+        { heading: "hexagon", src: path + "hexagon.jpg" },
+        { heading: "hexagons", src: path + "hexagons.jpg" },
+        { heading: "octagon", src: path + "octagon.jpg" },
+        { heading: "octagons", src: path + "octagons.jpg" }
+      ]
     };
+  },
+  beforeMount() {
+    this.art.sort((a, b) => {
+      return 0.5 - Math.random();
+    });
   },
   created() {
     setInterval(() => {
-      this.heading = this.headings[
-        Math.floor(this.headings.length * Math.random())
-      ];
+      this.heading = this.art[
+        Math.floor(this.art.length * Math.random())
+      ].heading;
     }, 3000);
   }
 };
@@ -81,20 +80,20 @@ body {
     "b b b b b b b b"
     "b b b b b b b b"
     "b b b b b b b b"
-    "f f f f f f c c"
-    "f f f f f f c c"
-    "f f f f f f d d"
-    "f f f f f f d d"
-    "f f f f f f e e"
-    "f f f f f f e e"
-    "l l l l m m m m"
-    "l l l l m m m m"
-    "l l l l m m m m"
-    "l l l l m m m m"
-    "g g h h h h k k"
-    "g g h h h h k k"
-    "j j h h h h i i"
-    "j j h h h h i i";
+    "c c c c c c d d"
+    "c c c c c c d d"
+    "c c c c c c e e"
+    "c c c c c c e e"
+    "c c c c c c f f"
+    "c c c c c c f f"
+    "g g g g h h h h"
+    "g g g g h h h h"
+    "g g g g h h h h"
+    "g g g g h h h h"
+    "i i k k k k l l"
+    "i i k k k k l l"
+    "j j k k k k m m"
+    "j j k k k k m m";
 
   align-content: center;
   justify-content: center;
